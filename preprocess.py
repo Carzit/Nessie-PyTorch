2fcb7b01be492265e25ee9ea0a4c449e8958e260
Carzit <126076978+Carzit@users.noreply.github.com>
2024-05-16 23:15:47 +0800

    Add files via upload

--- preprocess.py
+++ preprocess.py
@@ -4,18 +4,49 @@ import torch
 from torch.utils.data import DataLoader
 
 from datasets import NessieDataset, save_datasets, load_datasets
+from utils.data_utils import *
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="Split json Dataset and saved as pt File")
-
-    parser.add_argument("--data", type=str, required=True, help="Dataset `.json` File Path")
+    parser = argparse.ArgumentParser(description="Preprocessor. Load JSON file(s), do components split(in marginal distributions case), do train-val-test split and save datasets as .pt file(s).")
+    parser.add_argument("--data", type=str, required=True, nargs="+", help="Dataset `.json` File Path. Can pass one or more JSON file paths or pass filefolder path which contains all JSON fils")
     parser.add_argument("--save", type=str, required=True, help="Dataset `.pt` File path")
     parser.add_argument("--rate", type=int, nargs=3, default=[0.7, 0.2, 0.1], help="Train-Val-Test Split Rate")
     parser.add_argument("--split", type=bool, default=False, help="Split a Multi Component Dataset to Single Component Datasets")
 
     return parser.parse_args()
 
+def save_merged_data(json_files:list, save_path:str)->str:
+    print('Merging Data...')
+    merged_result = merge_json_files(json_files)
+    merged_path = f"{os.path.splitext(save_path)[0]}_merged.json"
+    with open(merged_path, 'w', encoding='utf-8') as output_file:
+        json.dump(merged_result, output_file, ensure_ascii=False, indent=4)
+    print(f"Data has been merged and saved to {merged_path}")
+    return merged_path
+
+
 def main(data_path, save_path, rate, split):
+    print(data_path)
+    if len(data_path) == 1:
+        data_path = data_path[0]       
+        path_flag = check_path(path=data_path, extension=".json")
+        if path_flag == 0:
+            raise ValueError(f"Unrecognizable path. Folder or file doesn't exist or extension doesn't match.")
+        if path_flag == 1:
+            pass
+        if path_flag == 2:
+            print(f'Folder path detected. All Json File in folder `{data_path}` will be loaded.')
+            json_files = get_files_with_extensions(data_path, extension=".json")
+            print(f"Total {len(json_files)} files: {json_files}")
+            data_path = save_merged_data(json_files, save_path)
+
+    elif len(data_path) >= 2:
+        print(f'Multiple paths detected. All Json File input will be loaded.')
+        json_files = list(data_path)
+        print(f"Total {len(json_files)} files: {json_files}")
+        data_path = save_merged_data(json_files, save_path)
+        
+
     print(f"Load Dataset from \"{data_path}\"")    
     info, file_path = save_datasets(data_path=data_path, save_path=save_path, save_format=".pt", split=rate)
     total_len = info[0]
@@ -29,8 +60,6 @@ def main(data_path, save_path, rate, split):
         for i in range(1, components):
             _, file_path = save_datasets(data_path=data_path, save_path=save_path+f"_{i}", save_format=".pt", split=rate, component_index=i)
             print(f"Component {i} Dataset Saved to \"{file_path}\"")
-
-
 
 if __name__ == "__main__":
     args = parse_args()
---
+++ utils/data_utils.py
@@ -0,0 +1,46 @@
+import os
+import json
+from pathlib import Path
+from typing import Union, List
+
+def get_extensions(extension:Union[str, List[str]]=None):
+    if isinstance(extension, str):
+        return [extension] if extension.startswith('.') else "."+extension
+    elif isinstance(extension, list):
+        return [item if item.endswith('.') else item + '.' for item in extension]
+    elif extension is None:
+        return None
+    else:
+        raise TypeError("Parameter 'extension' must be a string, a list of strings or None")
+
+def get_files_with_extensions(dir:str, extension:Union[str, List[str]]=None) -> List[str]:
+    current_dir = Path(dir)
+    extensions = get_extensions(extension)
+    if extensions is None:
+        files = [os.path.join(dir, file.name) for file in current_dir.iterdir()]
+    else:
+        files = [os.path.join(dir, file.name) for file in current_dir.iterdir() if file.is_file() and file.suffix in extensions]
+    return files
+
+def check_path(path:str, extension:Union[str, List[str]]=None):
+    path_obj = Path(path)
+    extensions = get_extensions(extension)
+    if path_obj.is_file() and (extensions is None or path_obj.suffix in extensions):
+        return 1 #->file
+    elif path_obj.is_dir():
+        return 2 #->folder
+    else:
+        return 0 #->unrecognized
+
+def merge_json_files(file_list:List):
+    merged_data = {'data_X': [], 'data_Y': []}
+
+    for file_name in file_list:
+        with open(file_name, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            if 'data_X' in data:
+                merged_data['data_X'].extend(data['data_X'])
+            if 'data_Y' in data:
+                merged_data['data_Y'].extend(data['data_Y'])
+
+    return merged_data
